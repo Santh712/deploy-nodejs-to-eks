378853ef01d680106faaf0835539b653eb37cfbe
Santh712 <121021602+Santh712@users.noreply.github.com>
2023-10-26 16:55:48 +0530

    Update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World, Gud Day to Everyone');
+  res.send('Hello World, Gud Day');
 });
 
 app.listen(PORT, HOST, () => {
